02d32816bb908c922e85409a6933fa04b2e113c1
ac127473 <malte.heckelen@ilw.uni-stuttgart.de>
2018-02-11 12:59:52 +0100

    security commit; working on rc_model.R

--- testbed.R
+++ testbed.R
@@ -1,6 +1,5 @@
 
-
-environment <- create_lattice(c(10, 10)) 
+## PREP FROM basic_setup.R
 
 no_agents <- 10
 epsilon <- 0.1
@@ -12,49 +11,89 @@ agent_characteristics <- tibble(
   
 )
 
-strategies_overall <- tibble(
+## PREP FROM small_world.R
+
+environment <- play_smallworld(n_dim=1, dim_size=no_agents, order=3, p_rewire=0.1)
+
+environment <- environment %>%
+  activate(nodes) %>%
+  mutate(agent_id = agent_characteristics$agent_id)
+
+agent_characteristics <- environment %>%
+  activate(nodes) %>%
+  mutate(neighborhood = local_members(mindist = 1)) %>%
+  mutate(nbh_size = local_size(mindist = 1)) %>%
+  as_tibble() %>%
+  select(agent_id, neighborhood, nbh_size) %>%
+  inner_join(agent_characteristics)
+
+# INIT
+
+# add tibbles for available actions and utility scores
+actions_overall <- tibble(
   
-  strategies = c("Receive", "Send", "Both", "Nothing"),
-  score = c(0, 0, 0, 0)
+  agent_id = rep(agent_characteristics$agent_id, each=4),
+  actions = rep(c("Receive", "Send", "Both", "Nothing"), each=4),
+  score = rep(0, length(agent_characteristics$agent_id))
   
 )
 
-strategies_send <- tibble(
+actions_send <- tibble(
   
+  agent_id = rep(agent_characteristics$agent_id, each=2),
   strategies = c("Unoptimized", "Optimized"),
-  score = c(0, 0)
+  score = rep(0, length(agent_characteristics$agent_id))
   
 )
 
-produce_altered_message <- function(opinion_send, opinion_receive) {
-  
-  # produce altered message without epsilon bound
-  altered_message <- ifelse(opinion_send < opinion_receive, 
-                            opinion_send + abs(opinion_send - opinion_receive) / 2, 
-                            opinion_send - abs(opinion_send - opinion_receive) / 2)
-  
-  # check if within epsilon to own opinion and correct
-  altered_message <- ifelse(abs(opinion_send - altered_message) > epsilon,
-                            ifelse((opinion_send - altered_message) <= 0, 0, opinion_send - altered_message),
-                            altered_message)
-  
-  return(altered_message)
-  
-} # works
 
+
+
+### Distances
+
+distances_part <- environment %>%
+  activate(edges) %>%
+  as_tibble() %>%
+  mutate(from_two = from) %>%
+  mutate(to_two = to) %>%
+  mutate(from = to_two) %>%
+  mutate(to = from_two) %>%
+  select(from, to)
+
+# create a distance table
+distances_table <- environment %>%
+  activate(edges) %>%
+  as_tibble() %>%
+  rbind(distances_part) %>%
+  inner_join(agent_characteristics, by=c("from" = "agent_id")) %>% 
+  mutate(opinion_from = opinion) %>%
+  mutate(assumption_to = vec_produce_initial_assumption(from, to))
+
+agent_characteristics <- agent_characteristics %>%
+  inner_join(distances_table, by=c("agent_id" = "from")) %>%
+  group_by(agent_id) %>%
+  mutate(no_within = sum(within_epsilon)) %>%
+  filter(within_epsilon == TRUE) %>%
+  mutate(opinion = ifelse(no_within != 0, sum(opinion_to) / no_within, opinion)) %>%
+  ungroup() %>%
+  select(agent_id, opinion) %>% 
+  full_join(agent_characteristics, by="agent_id") %>% 
+  mutate(opinion = coalesce(opinion.x, opinion.y)) %>%
+  select(agent_id, opinion) %>%
+  distinct()
+
+# STEP
 # make matrix of all possible optimized messages
 message_matrix <- outer(agent_characteristics$opinion, agent_characteristics$opinion, produce_altered_message) # works
 row.names(message_matrix) <- seq(1, no_agents, 1)
 colnames(message_matrix) <- seq(1, no_agents, 1)
-
 
 message_tibble <- message_matrix %>% 
   as_tibble() %>% 
   mutate(sender = row.names(message_matrix)) %>% 
   gather(receiver, message, "1":"10") # works
 
-
-agent_characteristics$opinion <- runif(no_agents, 0, 1)
+### Distances
 
 distances_part <- environment %>%
   activate(edges) %>%
@@ -93,41 +132,50 @@ agent_characteristics <- agent_characteristics %>%
   select(agent_id, opinion) %>%
   distinct()
 
-# OLD
+# FUNCTIONS
 
+# generate first assumed opinions (for Init)
+produce_initial_assumption <- function(sender, receiver) {
+  
+  opinion <- filter(agent_characteristics, agent_id == sender)$opinion
+  
+  sender_neighborhood <- unlist(filter(agent_characteristics, agent_id == sender)$neighborhood)
+  receiver_neighborhood <- unlist(filter(agent_characteristics, agent_id == receiver)$neighborhood)
+  
+  # generate
+  assumed_opinion <- sapply(unlist(sender_neighborhood), function(x) {
+    
+    size_intersect <- length(intersect(sender_neighborhood, receiver_neighborhood))
+    
+    if (runif(1, 0, 1) < 0.5) {
+      (opinion + epsilon) / size_intersect
+    } else {
+      (opinion - epsilon) / size_intersect
+    }
+    
+    
+  })
+  
+  return(assumed_opinion)
+  
+} # works
 
-distances_part <- sim$environment %>%
-  activate(edges) %>%
-  as_tibble() %>%
-  mutate(from_two = from) %>%
-  mutate(to_two = to) %>%
-  mutate(from = to_two) %>%
-  mutate(to = from_two) %>%
-  select(from, to)
+vec_produce_initial_assumption <- Vectorize(produce_initial_assumption) # works
 
-# create a distance table
-sim$distances_table <- sim$environment %>%
-  activate(edges) %>%
-  as_tibble() %>%
-  rbind(distances_part) %>%
-  inner_join(sim$agent_characteristics, by=c("from" = "agent_id")) %>%
-  mutate(opinion_from = opinion) %>%
-  select(from, to, opinion_from)
-
-sim$distances_table <- sim$distances_table %>%
-  inner_join(sim$agent_characteristics, by=c("to" = "agent_id")) %>%
-  mutate(opinion_to = opinion) %>% 
-  select(from, to, opinion_from, opinion_to) %>%
-  mutate(distance = opinion_from - opinion_to) %>%
-  mutate(distance = abs(distance)) %>%
-  mutate(within_epsilon = distance < params(sim)$hegselmann_krause$epsilon)
-
-print(sim$distances_table)
-print(sim$agent_characteristics) # NEXT STEP: DOUBLED COLUMNS NEED TO BE SINGULAR
-# create number of all agents within epsilon in agent_characteristics as length of boolean vector within_epsilon
-# compute new opinion vector
-sim$agent_characteristics <- sim$agent_characteristics %>%
-  inner_join(sim$distances_table, by=c("agent_id" = "from")) %>%
-  group_by(agent_id) %>%
-  mutate(no_within = sum(within_epsilon)) %>%
-  mutate(opinion = sum(distance) / no_within)
+# generate altered optimized messages to be taken into consideration
+produce_altered_message <- function(opinion_send, message_receive) {
+  
+  # produce altered message without epsilon bound
+  altered_message <- ifelse(opinion_send < message_receive, 
+                            opinion_send + abs(opinion_send - message_receive) / 2, 
+                            opinion_send - abs(opinion_send - message_receive) / 2)
+  
+  # check if within epsilon to own opinion and correct
+  # commenting this for now, see Evernote: Design
+  #altered_message <- ifelse(abs(opinion_send - altered_message) > epsilon,
+  #                          ifelse((opinion_send - altered_message) <= 0, 0, opinion_send - altered_message),
+  #                          altered_message)
+  
+  return(altered_message)
+  
+} # works
